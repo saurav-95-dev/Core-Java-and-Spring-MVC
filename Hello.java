214ff5a730532c11cbd83c4309e095f5386b9655
Saurabh <devsaurabh086@gmail.com>
2025-06-29 20:39:22 +0530

    Multi-dimen-array-1

--- Hello.java
+++ Hello.java
@@ -1,14 +1,18 @@
-//JAVA ARRAYS : 
 public class Hello {
     public static void main(String[] args) {
-        int nums[] = new int[10];
-        nums[0] = 1;
-        nums[1] = 12;
-        nums[2] = 2;
-        nums[3] = 8;
-        nums[4] = -3;
+        //Multidimensional array
+        int nums[][] = new int[3][4];
+
         for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
+
+            for (int j = 0; j < nums[i].length; j++) {
+                System.out.print(nums[i][j] + " c");
+            }
+
+            System.out.println();
+            
         }
+    
+       
     }
 }
