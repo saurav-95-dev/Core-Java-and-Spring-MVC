078f3c17df3924a4ff81d81083e0bb4a01093933
Saurabh <devsaurabh086@gmail.com>
2025-07-05 20:22:43 +0530

    arrays basics java

--- Hello.java
+++ Hello.java
@@ -1,1 +1,12 @@
-//Strings in java : 
+//Arrays in java : 
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Learning arrays : ");
+    }
+    //defining arrays : 
+    int arr[] = { 1, 2, 3, 45 };
+    int arr1[] = new int[40];
+
+}
