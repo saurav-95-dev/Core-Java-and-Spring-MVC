77e15361437004f78a95e72948cebf4833cbb425
Saurabh <devsaurabh086@gmail.com>
2025-07-05 20:35:42 +0530

    Array basics(Multidimensional)

--- Hello.java
+++ Hello.java
@@ -3,10 +3,28 @@
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Learning arrays : ");
-    }
-    //defining arrays : 
-    int arr[] = { 1, 2, 3, 45 };
-    int arr1[] = new int[40];
+    
+        //Multi-dimensional array :
+        //Asigining value via Math.random : 
+        
+        int arr[][] = new int[3][4];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                arr[i][j] = (int)(Math.random() * 10);
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < 4; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+            
+        }
+     
+     
+ }
 
 }
