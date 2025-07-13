256aa8d0b75b25300046e8995d2e1c88591cca6c
Saurabh <devsaurabh086@gmail.com>
2025-07-13 17:09:08 +0530

    Method-overiding-01

--- Hello.java
+++ Hello.java
@@ -1,24 +1,26 @@
+//Implement method overiding in java :
 
-public class Hello {
+class A {
+    public void show() {
+        System.out.println("Inside class A show ");
+    }
+}
+
+class B extends A {
+    public void show1() {
+        System.out.println("Inside  class B show");
+    }
+}
+
+class Hello {
     public static void main(String[] args) {
 
-        System.out.println("Introduction to inheritance");
-        Calculator obj1 = new Calculator();
+        System.out.println("Method overiding");
+         B obj1 = new B();
+         obj1.show();
 
-        int result1 = obj1.add(5, 4);
-        int result2 = obj1.sub(8, 5);
 
-        System.out.println(result1);
-        System.out.println(result2);
-        
-        AdvancedCalculator obj2 = new AdvancedCalculator();
-        int ans1 = obj2.add(1, 2);
-        int ans2 = obj2.sub(4, 3);
-        int ans3 = obj2.mul(4, 3);
-        int ans4 = obj2.div(4, 2);
-        System.out.println(".................................");
-
-        System.out.println(ans1 + "," + ans2+ "," + ans3+ "," + ans3 + "," + ans4);
-      
     }
+
+   
 }
