25ba1ee5d7f8810f2f38b0ba5c1ab374ff21fbda
Saurabh <devsaurabh086@gmail.com>
2025-07-24 21:45:20 +0530

    Inner class-01

--- Hello.java
+++ Hello.java
@@ -1,1 +1,29 @@
-//Dynamic method dispatch : 
+//CONCEPT OF INNER CLASS : 
+
+
+class A {
+    int num;
+
+    void show() {
+        System.out.println("Inside Show");
+    } //Accessible within the same class.
+
+    class B {
+        int num2;
+
+        void config() {
+            System.out.println("Inside config");
+        }
+    }
+}
+
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Inside Hello Method");
+        A obj = new A();
+        obj.show();
+
+        A.B obj1 = obj.new B();
+        obj1.config();
+    }
+}
