1d60d60b01f60edd2a25dbafb216a1db3f8e9934
Saurabh <devsaurabh086@gmail.com>
2025-07-29 23:45:57 +0530

    Inner-class-02

--- Hello.java
+++ Hello.java
@@ -2,3 +2,28 @@
 // An Inner class can only access static members and methods of the outer class directly.
 // How we can we access non-static members and method of outer class inside inner class.
 
+class Saurabh {
+
+    int num1;
+
+    public void saurabhShow() {
+        System.out.println("Thisis outer class");
+    }
+
+    class Dev {
+        
+        int num2;
+
+        public void devShow() {
+            System.out.println("This is inner class");
+        }
+    }
+    
+}
+
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("This is main method");
+
+    }
+}
