75c7cfa465e1f0023f04a229ea234f8ccdeb7578
Saurabh <devsaurabh086@gmail.com>
2025-07-31 23:25:49 +0530

    Reset-01

--- Hello.java
+++ Hello.java
@@ -4,11 +4,5 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("This is main method");
 
-        Saurabh obj = new Saurabh();
-        obj.saurabhShow();
-
-        Saurabh.Dev obj1 = new Saurabh.Dev();
-        obj1.devShow();
-
     }
 }
