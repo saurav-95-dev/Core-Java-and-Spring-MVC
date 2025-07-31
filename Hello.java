863b0cb8e4a918a18c37a1295b624b1ee2900da4
Saurabh <devsaurabh086@gmail.com>
2025-07-31 23:25:26 +0530

    Reset-01

--- Hello.java
+++ Hello.java
@@ -1,28 +1,4 @@
 
-class Saurabh {
-
-
-    int num1=11;
-    static int number = 12;
-
-    public void saurabhShow() {
-        System.out.println("This is outer class");
-    }
-
-    static class Dev {
-        
-        int num2;
-
-        public void devShow() {
-            System.out.println("This is inner class");
-            System.out.println(number);
-            Saurabh o = new Saurabh();
-            System.out.println(o.num1);
-
-        }
-    }
-    
-}
 
 public class Hello {
     public static void main(String[] args) {
