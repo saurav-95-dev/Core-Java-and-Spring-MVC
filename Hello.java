b6f6cb015ae97c0b9fb06e57dd503c2bc2ad1a09
Saurabh <devsaurabh086@gmail.com>
2025-08-10 22:51:28 +0530

    Java-Foundation-01

--- Hello.java
+++ Hello.java
@@ -1,8 +1,13 @@
-
-
+//Concept of classes and objects:
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("This is main method");
-
+        System.out.println("Building Foundation in java");;
     }
 }
+//Takeaways:
+//In java , in order to create an object , you first have to create a class.
+//Java Byte code is platform independent and can run on any OS that has JRE.
+//JRE has a software component called JVM that converts java byte code into native code for the unline OS.
+//We create a blueprint by creating a class which turns into bytecode . This bytecode pass into JVM from where
+// we get our object.
+
