881ef51362f790b5cde0eab319eb0f4ef084ed2d
Saurabh <devsaurabh086@gmail.com>
2025-08-15 11:29:53 +0530

    Foundation-InternalJavaWorking

--- Hello.java
+++ Hello.java
@@ -1,9 +1,24 @@
 //Concept of classes and objects:
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Building Foundation in java");;
+class Calculator{
+
+    public int add(int a,int b){
+        return a+b;
     }
 }
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Building Foundation in java");
+        //Create a calculator to add two number:
+        Calculator c = new Calculator();
+        int result = c.add(5,2);
+
+        System.out.println(result);
+    }
+}
+
+
+
+
 //Takeaways:
 //In java , in order to create an object , you first have to create a class.
 //Java Byte code is platform independent and can run on any OS that has JRE.
