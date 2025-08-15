a641408212e758cac8fe1e641041c0a945cce9d2
Saurabh <devsaurabh086@gmail.com>
2025-08-15 12:47:31 +0530

    Foundation-02-Concept of Heap-memory and variable-types

--- Hello.java
+++ Hello.java
@@ -1,23 +1,26 @@
 //Concept of classes and objects:
 
-class Calculator{
-    int num;
-    public int add(int a , int b){
-        return a+b;
+class Demo{
+
+    int num; //This is instance variable
+
+    public String playMusic(int cost){
+        //Here , cost is a local variable.
+        if(cost >= 10){
+            return "Playing music";
+        }
+        return "Nothing";
     }
 }
 public class Hello{
     public static void main(String[] args) {
-        Calculator c = new Calculator();
-        int result = c.add(2,3);
-        System.out.println(result);
+         //Here demo is a reference variable.
+         Demo demo = new Demo();
+         String str = demo.playMusic(5);
+         System.out.println(str);
+
     }
 }
-
 //Takeaways:
-//In java , in order to create an object , you first have to create a class.
-//Java Byte code is platform independent and can run on any OS that has JRE.
-//JRE has a software component called JVM that converts java byte code into native code for the unline OS.
-//We create a blueprint by creating class which gets compiled into bytecode . This bytecode pass into JVM from where
-// we get our object.
-
+//Instance variable gets space inside Heap-memory.
+//Reference variable are inside the stack which points to the address of the corresponding object that is created in the Heap.
