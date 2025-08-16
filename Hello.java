232a8ff99dfbe16f06d4994c3643b8063d91db3f
Saurabh <devsaurabh086@gmail.com>
2025-08-16 14:16:53 +0530

    Static-Keyword-01

--- Hello.java
+++ Hello.java
@@ -1,12 +1,35 @@
-//Mutable Strings in java:
-//StringBuffer and StringBuilder.
-//StringBuffer is Thread saved and StringBuilder is not.
+//Concept of static keyword:
 
-public static void main(String[] args){
-    StringBuilder sb1 = new StringBuilder("Saurabh");
-    sb1.append(" Dev,");
-    sb1.insert(12, "How r u doing..!");
-    System.out.println(sb1);
+class Demo{
+    //instance variables:
+    String name;
+    int marks;
+    String grade;
+    static String branch;
 
+    public void show(){
+        System.out.println("Name:" + name + " Marks:" + marks + " Grade:" + grade + " Branch:" + branch);
+    }
 }
 
+public class Hello{
+    public static void main(String[] args){
+        Demo d1 = new Demo();
+        d1.name = "Saurabh Dev";
+        d1.marks = 100;
+        d1.grade = "A";
+        //d1.branch = "IT";
+        Demo d2 = new Demo();
+        d2.name="Babu Raj Singh";
+        d2.marks = 200;
+        d2.grade = "B";
+        Demo.branch= "CS";
+       // d1.branch = "IT";
+        d2.show();
+        d1.show();
+
+    }
+}
+
+//Takeaways:
+//There is no need to create instance of the static members/methods , we can call them directly from the name of class itself.
