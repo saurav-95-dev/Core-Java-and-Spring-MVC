153e4b198c6c1dad6c9990f48f4dfd402d17d715
Saurabh <devsaurabh086@gmail.com>
2025-08-17 12:28:23 +0530

    Encapsulation Getters-Setters

--- Hello.java
+++ Hello.java
@@ -1,26 +1,37 @@
 //Encapsulation in Java:
 
 class Computer{
-     private int ram = 12;
-     private String company = "HP";
+     private int ram;
+     private String company;
 
      public void showModel1(){
          System.out.println("This is model-1");
-
      }
 
+     //Getters:
      public int getModel1Ram(){
          return ram;
      }
      public String getModel1Company(){
          return company;
      }
+
+     //Setters:
+     public void setModel1Ram(int ram ){
+         this.ram = ram;
+     }
+     public void setModel1Company(String company){
+         this.company = company;
+     }
+
 }
 
 class Hello{
     public static void main(String[] args){
          Computer computer = new Computer();
          computer.showModel1();
+         computer.setModel1Ram(122);
+         computer.setModel1Company("Lenovo");
         System.out.println("Ram:" + computer.getModel1Ram() + "\nCompany:" + computer.getModel1Company());
 
     }
