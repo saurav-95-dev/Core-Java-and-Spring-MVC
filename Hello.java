4e1759059b57da65eaed205791091797dad2a734
Saurabh <devsaurabh086@gmail.com>
2025-08-17 16:44:44 +0530

    Parameterized constructor

--- Hello.java
+++ Hello.java
@@ -7,11 +7,17 @@ class Student{
 
    //Default constructor:
     public Student(){
-        System.out.println("Inside Constructor");
+        System.out.println("Inside Default Constructor");
        this.age = 10;
        this.marks = 80;
        this.name = "NA";
        this.branch = "IT";
+    }
+
+    public Student(int age , String branch){
+        System.out.println("Inside Parameterized constructor");
+        this.age = age;
+        this.branch = branch;
     }
 }
 
@@ -19,6 +25,8 @@ public class Hello{
     public static void main(String[] args){
          Student s1 = new Student();
          Student s2 = new Student();
+         Student s3 = new Student(22,"CS");
          System.out.println(s1.age + "\n" + s1.marks + "\n" + s1.name + "\n" + s1.branch);
+         System.out.println(s3.age + "\n" + s3.marks + "\n" + s3.branch);
     }
 }
