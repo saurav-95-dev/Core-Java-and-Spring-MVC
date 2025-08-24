8ab37a6be8a5c02de5b03c8a7090380ed76d41dc
Saurabh <devsaurabh086@gmail.com>
2025-08-24 19:55:16 +0530

    Inheritance02-24/08/2025

--- Hello.java
+++ Hello.java
@@ -2,35 +2,22 @@
 //Note : Multiple Inheritance is not supported in java.
 
 class Calculator{
-
     public int add(int a , int b){
         return a+b;
     }
     public int sub(int a , int b){
         return a-b;
     }
-
-}
-
-class AdvCalculator extends Calculator{
-    public int mul(int a , int b){
-        return a*b;
-    }
-    public int div(int a , int b){
-        return a/b;
-    }
-
 }
 
 public class Hello{
 
     public static void main(String[] args){
-        Calculator obj1 = new Calculator();
-        System.out.println(obj1.add(1,2));
-        AdvCalculator obj2 = new AdvCalculator();
-        System.out.println(obj2.add(8,2));
-
-
+          AdvCalculator calc = new AdvCalculator();
+          int result1 = calc.add(1,2);
+          int result2 = calc.mul(4,5);
+          System.out.println("Addition :" + result1);
+          System.out.println("Multiplication :" + result2);
     }
 
 }
