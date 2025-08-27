3a704c2f35388472e672d29834a2dcf1f9d1b22a
Saurabh <devsaurabh086@gmail.com>
2025-08-27 14:39:23 +0530

    Object-class02-27/08/2025

--- Hello.java
+++ Hello.java
@@ -1,19 +1,30 @@
 //Object classes in brief:
 //Everytime if we try to print an object , it calls a toString() method .
+//In java if we are trying to check for the equality of 2 objects , they should have same value as well as same hashcode.
 
 class Computer{
     int marks;
     String name;
+
     public String toString(){
         return "Hii there";
     }
+
+    public boolean equals(Computer that){
+        return this.name.equals(that.name) && this.marks == that.marks;
+    }
+
 }
 
 public class Hello{
     public static void main(String [] args){
-         Computer c = new Computer();
-         c.name = "Saurabh Dev";
-         c.marks = 100;
-         System.out.println(c.toString());
+         Computer c1 = new Computer();
+         c1.name = "Saurabh Dev";
+         c1.marks = 100;
+         Computer c2 = new Computer();
+         c2.name = "Saurabh Dev";
+         c2.marks = 100;
+         System.out.println(c1);
+        System.out.println(c1.equals(c2));
     }
 }
