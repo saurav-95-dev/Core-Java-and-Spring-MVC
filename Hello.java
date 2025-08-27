9368826ac43e8cc6d4a8092a3b8741f659f05655
Saurabh <devsaurabh086@gmail.com>
2025-08-27 16:46:37 +0530

    Upcasting-Downcasting

--- Hello.java
+++ Hello.java
@@ -1,30 +1,28 @@
-//Object classes in brief:
-//Everytime if we try to print an object , it calls a toString() method .
-//In java if we are trying to check for the equality of 2 objects , they should have same value as well as same hashcode.
+//Upcasting and Downcasting .
+//Upcasting is automatic , Downcasting is explicit .
 
 class Computer{
     int marks;
     String name;
-
-    public String toString(){
-        return "Hii there";
+    public void func1(){
+        System.out.println("Inside Computer class");
     }
+}
 
-    public boolean equals(Computer that){
-        return this.name.equals(that.name) && this.marks == that.marks;
+class Laptop extends Computer{
+    public void func2(){
+        System.out.println("Inside Lenovo class");
     }
-
 }
 
 public class Hello{
     public static void main(String [] args){
-         Computer c1 = new Computer();
-         c1.name = "Saurabh Dev";
-         c1.marks = 100;
-         Computer c2 = new Computer();
-         c2.name = "Saurabh Dev";
-         c2.marks = 100;
-         System.out.println(c1);
-        System.out.println(c1.equals(c2));
+//        Even though at runtime, c1 is pointing to a Laptop object, the compiler
+//        doesn’t allow calling func2() with c1 because the declared reference type (Computer) doesn’t know about it.
+          Computer c1 = new Laptop();
+          Laptop l = (Laptop)c1; // DownCasting
+//By down-casting we are telling java , "Hey, trust me, this Computer reference actually points to a Laptop object."
+          l.func2();
+
     }
 }
