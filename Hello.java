ced4dd3f4b749084cfe7ccb0cd63e702fd7b2bd1
Saurabh <devsaurabh086@gmail.com>
2025-08-27 21:00:44 +0530

    InnerClass object when inner class is non-static

--- Hello.java
+++ Hello.java
@@ -1,31 +1,26 @@
-//Abstract Keyword in Java:
+//Inner class:
 //NOTES:
-//abstract method can only belong to an abstract class.
-//It is not possible to create the object of an abstract class but it is ok to create a reference of it.
-//It is necessary to have an abstract class for an abstract method but reverse os not true.
-//It is not necessary to have an abstract method inside an abstract class.
-//It is necessary to implement the abstract methods inside the child class which is also called concrete class.
+//We can't use static keyword for the outer class . It can be used with only inner class.
 
-abstract class Car{
-
-    public abstract void drive();
-    public void playMusic(){
-        System.out.println("I'm playing music");
+class Computer{
+    public void func1(){
+        System.out.println("Inside Computer class..");
     }
-
+    class Laptop{
+        public void func2(){
+            System.out.println("Inside inner class Laptop..");
+        }
+    }
 }
 
-class Iten extends Car{
-    public void drive(){
-        System.out.println("I'm driving");
-    }
 
-}
 
-public class Hello{
-    public static void main(String [] args){
-       Car car = new Iten();
-       car.drive();
-       car.playMusic();
+class Hello{
+    public static void main(String[] args) {
+         //For Creating an object of inner class , we need the object of outer class first.
+         Computer c = new Computer();
+         Computer.Laptop c1 = c.new Laptop();
+         c1.func2();
+
     }
 }
