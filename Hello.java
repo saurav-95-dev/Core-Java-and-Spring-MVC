7c6b6eac29f1e9b3b4ce4942ce58ff3eac30d6a7
Saurabh <devsaurabh086@gmail.com>
2025-08-27 14:27:02 +0530

    Object-class01-27/08/2025

--- Hello.java
+++ Hello.java
@@ -1,25 +1,19 @@
-//Concept of Final Keyword. --> Can be used with variable , method , class.
-//For variable , final is used to make it constant.
-//For class , final is used to prevent inheritance i.e , no-one can extend it .
-//In case of method , final is used to prevent method overriding.
+//Object classes in brief:
+//Everytime if we try to print an object , it calls a toString() method .
 
 class Computer{
-    final int num = 12;
-    public final void func1(){
-        System.out.println("Inside Computer class");
-        System.out.println(num);
+    int marks;
+    String name;
+    public String toString(){
+        return "Hii there";
     }
 }
 
-class Laptop extends Computer{
-    public void func1(){
-        System.out.println("Inside Laptop class");
-    }
-}
-
-public class Hello {
-    public static void main(String[] args) {
-       Computer c = new Computer();
-       c.func1();
+public class Hello{
+    public static void main(String [] args){
+         Computer c = new Computer();
+         c.name = "Saurabh Dev";
+         c.marks = 100;
+         System.out.println(c.toString());
     }
 }
