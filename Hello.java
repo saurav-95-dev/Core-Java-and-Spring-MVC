e9995246fb88783ad8a571c9dac481996d299a6a
Saurabh <devsaurabh086@gmail.com>
2025-08-29 22:49:08 +0530

    new change-02-29/08/2025

--- Hello.java
+++ Hello.java
@@ -4,13 +4,6 @@
 class Hello{
     public static void main(String[] args) {
 
-         Computer c = new Computer()
-         {
-             public void func1(){
-                 System.out.println("Inside Modified Computer class..");
-             }
-         };
-         c.func1();
 
     }
 }
