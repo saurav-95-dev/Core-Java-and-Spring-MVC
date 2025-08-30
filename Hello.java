15325fd32dca923160845d041f6243a53ccd3ac0
Saurabh <devsaurabh086@gmail.com>
2025-08-30 21:03:02 +0530

    Inheritance in interface-30/08/2025

--- Hello.java
+++ Hello.java
@@ -8,6 +8,7 @@
 //interface will only give design-idea of method . It is the duty of class which is implementing it to actually define the methods inside it ,
 // because , we create the object of class and not the interface.
 //It becomes necessary to implement the methods that are declared inside interface in the  class which is  extending it otherwise that class will become abstract class by-default.
+//In abstract class we can extend only 1 class , but we can have a class implementing multiple interfaces.
 
 interface Machine{
     int marks = 12;   //Final and static by default
@@ -16,12 +17,26 @@ interface Machine{
     void config();
 }
 
-class Laptop implements Machine{
+interface Human{
+    void run();
+}
+
+interface Man extends Human{
+    void hasMuscles();
+}
+
+class Laptop implements Machine , Man{
      public void show(){
          System.out.println("Inside show method");
      }
      public void config(){
          System.out.println("Inside config method");
+     }
+     public void run(){
+         System.out.println("Running..");
+     }
+     public void hasMuscles(){
+         System.out.println("Inside hasMuscles method");
      }
 }
 
@@ -32,6 +47,7 @@ class Hello {
        Machine machine = new Laptop();
        machine.config();
        machine.show();
+
         System.out.println(Machine.marks);
         System.out.println(machine.name);
     }
