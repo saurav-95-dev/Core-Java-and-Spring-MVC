38c6578e515af609ebbcd65d1f0c5bf1598f8aeb
Saurabh <devsaurabh086@gmail.com>
2025-08-30 20:51:37 +0530

    Interfaces-01-30/08/2025

--- Hello.java
+++ Hello.java
@@ -1,1 +1,35 @@
 //Concept of interfaces in java:
+//NOTES:
+//Inside abstract class , we can or cannot create an abstract method. We can just create normal methods as well.
+//When we have a class that has only abstract methods , u can create it as interface.
+//Every method inside an interface is "public abstract".
+//Every variable inside interface is by-default final and static.
+//Remember compile-time rule -> "You can only call methods that the reference type knows about."
+//interface will only give design-idea of method . It is the duty of class which is implementing it to actually define the methods inside it ,
+// because , we create the object of class and not the interface.
+//It becomes necessary to implement the methods that are declared inside interface in the  class which is  extending it otherwise that class will become abstract class by-default.
+
+interface Machine{
+    void show();
+    void config();
+}
+
+class Laptop implements Machine{
+     public void show(){
+         System.out.println("Inside show method");
+     }
+     public void config(){
+         System.out.println("Inside config method");
+     }
+}
+
+
+class Hello {
+
+    public static void main(String[] args) {
+       Machine machine = new Laptop();
+       machine.config();
+       machine.show();
+    }
+
+}
