2f2f0f0ae33a695eeeb69985dff173e0c8e0c728
Saurabh <devsaurabh086@gmail.com>
2025-08-30 23:27:38 +0530

    Need for the interface-01

--- Hello.java
+++ Hello.java
@@ -1,62 +1,37 @@
-//Concept of interfaces in java:
-//NOTES:
-//Inside abstract class , we can or cannot create an abstract method. We can just create normal methods as well.
-//When we have a class that has only abstract methods , u can create it as interface.
-//Every method inside an interface is "public abstract".
-//Every variable inside interface is by-default final and static.
-//Remember compile-time rule -> "You can only call methods that the reference type knows about."
-//interface will only give design-idea of method . It is the duty of class which is implementing it to actually define the methods inside it ,
-// because , we create the object of class and not the interface.
-//It becomes necessary to implement the methods that are declared inside interface in the  class which is  extending it otherwise that class will become abstract class by-default.
-//In abstract class we can extend only 1 class , but we can have a class implementing multiple interfaces.
-// Class - class --> extends , class - interface --> implements , interface-interface -> extends again.
-
-interface Machine{
-    int marks = 12;   //Final and static by default
-    String name = "Saurabh";   //Final and static by default
-    void show();
-    void config();
-    //void run();
-}
-
-interface Human extends Machine{
-    void run();
-}
-
-interface Man extends Human{
-    void hasMuscles();
-}
-
-class Laptop implements Machine , Human {
-     public void show(){
-         System.out.println("Inside show method");
-     }
-     public void config(){
-         System.out.println("Inside config method");
-     }
-     public void run(){
-         System.out.println("Running..");
-     }
-     public void hasMuscles(){
-         System.out.println("Inside hasMuscles method");
-     }
-}
 
 
-class Hello {
-
-    public static void main(String[] args) {
-       Machine machine = new Laptop();
-       machine.config();
-       machine.show();
-
-       Human human = new Laptop();
-       machine.run();  //Here reference type Machine doesn't know about run method as it is not declares inside it .
-       //Here machine points to Laptop object in heap which does has the run() definition . But the compile time rule says - "You can only call methods that the reference type knows about."
-        human.run();
-
-        System.out.println(Machine.marks);
-        System.out.println(Machine.name);
+class Desktop{
+    public void useDesktop(){
+        System.out.println("Inside desktop method of Desktop class");
     }
+    public void writeCodeDesktop(){
+        System.out.println("Writing code using desktop");
+    }
+}
 
+class Laptop{
+    public void useLaptop(Desktop desktop){
+        desktop.useDesktop();
+    }
+    public void writeCodeLaptop(){
+        System.out.println("Writing code using laptop");
+    }
+}
+
+class Developer{
+    //Tight coupling as we are limit to receiving only Laptop object here.
+    public void buildApplication(Laptop laptop){
+        System.out.println("Software engineering");
+        laptop.writeCodeLaptop();
+    }
+}
+
+class Hello{
+    public static void main(){
+        Desktop desktop = new Desktop();
+        Laptop laptop = new Laptop();
+        Developer developer = new Developer();
+        //Tight coupling:
+        developer.buildApplication(dektop);
+    }
 }
