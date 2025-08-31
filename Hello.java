7d3e8a3ffa7634a70a215a9e77ac1e219da6477e
Saurabh <devsaurabh086@gmail.com>
2025-08-31 14:00:56 +0530

    Enum and annotations-01-31/08/2025

--- Hello.java
+++ Hello.java
@@ -1,48 +1,15 @@
+//Enums and annotations:
+//Enums are a kind of class in java , and all the named constant inside it can be called as the objects.
 
-interface Computer{
-
-    void buildApplication();
+enum Status{
+    running , waiting , failed , successful;
 }
 
-class Desktop implements Computer{
-    public void useDesktop(){
-        System.out.println("Inside desktop method of Desktop class");
-    }
-    public void writeCodeDesktop(){
-        System.out.println("Writing code using desktop");
-    }
-    public void buildApplication(){
-        System.out.println("Building application using desktop");
-    }
-}
+public class Hello {
 
-class Laptop implements Computer{
-    public void useLaptop(Desktop desktop){
-        desktop.useDesktop();
+    public static void main(String[] args){
+        Status s = Status.running;
+        System.out.println(s);
     }
-    public void writeCodeLaptop(){
-        System.out.println("Writing code using laptop");
-    }
-    public void buildApplication(){
-        System.out.println("Building application using laptop");
-    }
-}
 
-class Developer{
-    //Loose coupling as we are receiving the reference of Computer class interface which is getting implemented by both Laptop and Desktop class.
-    public void buildApplication(Computer c){
-        System.out.println("Software engineering");
-        c.buildApplication();
-    }
-}
-
-class Hello{
-    public static void main(){
-
-        Developer developer = new Developer();
-        //Loose coupling:
-        Computer desktop = new Desktop();
-        Computer laptop = new Laptop();
-        developer.buildApplication(laptop);
-    }
 }
