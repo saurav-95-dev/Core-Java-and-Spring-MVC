26882cb68b0c7b60fc104b6c339ba61e2802273a
Saurabh <devsaurabh086@gmail.com>
2025-09-01 22:12:10 +0530

    New change-03-1/09/2025

--- Hello.java
+++ Hello.java
@@ -1,27 +1,3 @@
-//Enums and annotations:
-//Enums are a kind of class in java , and all the named constant inside it can be called as the objects.
-//Switch is supported in enum.
-//Although Enum is a class , u cant extends enum in any other class. But we can define methods , constructor , variables.
-//By default , enum in java extends enum class . and all the extra methods we get from enum class.
-enum Laptop{
-    //Whatever the values we are passing here , will get assigned in private int price variable.
-    mackbook(2000), hp , thinkpad(300) , lenovo(800);
-    private int price;
-
-    //Parameterized constructor:
-    private Laptop(int price){
-        this.price = price;
-        System.out.println("This is parameterized constructor");
-    }
-
-    private Laptop(){
-        this.price = 200000;
-        System.out.println("This is default constructor");
-    }
-
-
-
-}
 
 class Hello{
 
