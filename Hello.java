b5af44850a9e5cea4da3f4841b61f8a29be201fc
Saurabh <devsaurabh086@gmail.com>
2025-09-01 22:12:34 +0530

    New change-04-1/09/2025

--- Hello.java
+++ Hello.java
@@ -2,15 +2,12 @@
 class Hello{
 
     public static void main(String[] args) {
-        Laptop l = Laptop.thinkpad;
+
         System.out.println(l);
 
         //printing all the laptop:
         System.out.println("Printing for all laptops:");
-        Laptop arr[] = Laptop.values();
-        for(Laptop i : arr){
-            System.out.println(i + " : " + i.getPrice());
-        }
+
     }
 
 }
