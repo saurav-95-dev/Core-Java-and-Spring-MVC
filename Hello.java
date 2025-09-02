90a1e6c310d11882b3216ae59caa0c2f3d09c9fa
Saurabh <devsaurabh086@gmail.com>
2025-09-02 23:30:21 +0530

    new change

--- Hello.java
+++ Hello.java
@@ -1,13 +1,1 @@
 
-class Hello{
-
-    public static void main(String[] args) {
-
-        System.out.println(l);
-
-        //printing all the laptop:
-        System.out.println("Printing for all laptops:");
-
-    }
-
-}
