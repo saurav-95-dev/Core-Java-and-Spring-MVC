80925c442af0fecb82b0402cbc48e1c858b82083
Saurabh <devsaurabh086@gmail.com>
2025-09-03 23:44:34 +0530

    First commit

--- Hello.java
+++ Hello.java
@@ -1,1 +1,16 @@
 
+class Hello{
+
+    public static void main(String[] args) {
+
+        System.out.println(l);
+
+        //printing all the laptop:
+        System.out.println("Printing for all laptops:");
+        Laptop arr[] = Laptop.values();
+        for(Laptop i : arr){
+            System.out.println(i + " : " + i.getPrice());
+        }
+    }
+
+}
