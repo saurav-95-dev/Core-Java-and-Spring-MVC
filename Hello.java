d76283577db16c4b3b31c417e78215bcfabb2f0a
Saurabh <devsaurabh086@gmail.com>
2025-09-07 17:40:05 +0530

    New change-abstraction

--- Hello.java
+++ Hello.java
@@ -1,35 +1,3 @@
 //Need for the interface:
 //For loose coupling.
 
-interface Computer{
-    void code();
-
-}
-
-class Laptop implements Computer{
-
-    public void code() {
-        System.out.println("Coding , debugging , deployment inside laptop");
-    }
-}
-
-class Desktop implements Computer{
-    public void code(){
-        System.out.println("Coding , debugging , deployment inside desktop");
-    }
-}
-
-class Developer{
-    public void codeAppliation(Computer l){
-        l.code();
-    }
-}
-
-class Hello{
-    static void main(String[] args) {
-        Developer developer = new Developer();
-        Computer l = new Laptop();
-        Computer d = new Desktop();
-        developer.codeAppliation(d);
-    }
-}
