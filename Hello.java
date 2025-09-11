4b0d246d20d592b7b639772dcfc3aa97ec95572e
Saurabh <devsaurabh086@gmail.com>
2025-09-11 23:31:47 +0530

    Wrapper classes in java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,19 @@
-//10/09/2025
-//Abstraction in java and the use of abstract keyword.
-//Interface in java.
+//11/09/2025
+//Wrapper class :
+//NOTES:
+//For every primitive type we have class for it and that class extends the object class.
+//Difference b/w primitive type and wrapper class :
+//1-> Primitive types are not objects and hence lack OOPS features like methods and fields.
+//2-> Primitive types cannot be assigned null values . They need to have some default values whereas wrapper class being objects , can hold null values.
+//3-> Primitive types are generally more efficient where wrapper class are not due to object overhead.
+//4-> Primitive types values can be changes directly , whereas instances of wrapper classes are generally immutable, meaning their value cannot be changed after creation. A new object is created for modifications.
 
 
+class Hello{
+    public static void main(String[] args){
+        System.out.println("Inside main function");
+
+    }
+}
 
 
