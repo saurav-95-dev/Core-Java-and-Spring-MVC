0f706042d8aa54e1ac74c009c48c9e050c38e1e0
Saurabh <devsaurabh086@gmail.com>
2025-09-13 17:57:54 +0530

    Abstraction further

--- Hello.java
+++ Hello.java
@@ -1,32 +1,48 @@
-//12/09/2025 : Abstraction and abstract keyword in java:
-//NOTES:
-//Compile time rule - You can only call methods about which reference type is aware of.
 
 
 abstract class Employee{
 
+    public abstract void coder();
+
+    public void receptionist(){
+        System.out.println("Receptionist method inside Employee class-non-abstract");
+    }
+
+    public void manager(){
+        System.out.println("manager method inside Employee class-non-abstract");
+    }
+    public abstract void dataAnalyst();
+}
+
+class Engineer extends Employee{
+    public void dataAnalyst(){
+        System.out.println("This is the implementation of dataAnalyst inside Engineer");
+    }
     public void coder(){
-        System.out.println("This is a coder method inside Employee class");
+        System.out.println("This is the implementation of coder inside Engineer");
     }
-    public abstract void analyst();
-    public abstract void manager();
-}
-
-class Helper extends Employee{  //Concrete class
-    public void analyst(){
-        System.out.println("This is the implementation of analyst method");
-    }
-    public void manager() {
-        System.out.println("This is the implementation of manager");
+    public void businessAnalyst(){
+        System.out.println("This is business analyst inside Engineer class which is not implementing any abstract class");
     }
 }
 
-class Hello{
-    public static void main(String [] arg){
-        Employee e = new Helper();
-        e.coder();
-        e.analyst();
+public class Hello{
+    public  static void main(String [] args){
+        System.out.println("Inside main function");
+        //Compile time rule -> You can only call methods which reference type knows about.
+        Employee e = new Engineer();
         e.manager();
+        e.dataAnalyst();
+        e.coder();
+        System.out.println();
+        System.out.println("Printing from Engineer reference");
+        Engineer e1 = new Engineer();
+        e1.businessAnalyst();
+        e1.coder();
+        e1.manager();
+        e1.receptionist();
+        e1.dataAnalyst();
+
 
     }
 }
