a8fae1dc9e9aa40601011e1d1dad3a607fc3f249
Saurabh <devsaurabh086@gmail.com>
2025-09-13 20:55:36 +0530

    Interface-01

--- Hello.java
+++ Hello.java
@@ -1,20 +1,29 @@
-//Abstraction  + Anonymous inner class.
-//->How can we create object of inner class which seems like creating the object of abstract class ?
-//->If there's only one-time implementation of the abstract method or interface of the abstract class, we can do it inside anonymous inner class.
+//Concept of interface:
+//They don't have their own memory in the heap.
+//All the methods inside the interface are by default public and abstract.
+//All the variables declared inside the interface are final and static.
+//Once we declare the methods inside the interface , its becomes compulsory to implement those methods inside the class which is implementing it.
+//If we fail to implement the methods of an interface inside the class which is extending it , then by default , that class also becomes an abstract class.
 
-abstract class A{
-    public abstract void aShow();
+interface A{
+    void show();
+    void config();
 }
 
-public class Hello{
-    public static void main(String[] args) {
+class B implements A{
+    public void show(){
+        System.out.println("Show method of B class which is implementing interface A");
+    }
+    public void config(){
+        System.out.println("Config method of B class which is implementing interface A");
+    }
+}
+
+class Hello{
+    public static void main() {
         System.out.println("Inside main function");
-        A a = new A()
-        {
-            public void aShow(){
-                System.out.println("aShow has been implemented");
-            }
-        };
-        a.aShow();
+        A obj = new B(); //Here we are creating the reference of A ,but object of B
+        obj.config();
+        obj.show();
     }
 }
