6c4f94b9825a0c59d01abdc3c56457c961fbb674
Saurabh <devsaurabh086@gmail.com>
2025-09-14 23:41:43 +0530

    Child-parent-reference-object creation

--- Hello.java
+++ Hello.java
@@ -1,47 +1,28 @@
-//Concept of interface:
-//They don't have their own memory in the heap.
-//All the methods inside the interface are by default public and abstract.
-//All the variables declared inside the interface are final and static.
-//Once we declare the methods inside the interface , its becomes compulsory to implement those methods inside the class which is implementing it.
-//If we fail to implement the methods of an interface inside the class which is extending it , then by default , that class also becomes an abstract class.
-//NOTE: Compile time rule says , that we can call object which reference type is aware of.
-//We cannot object of a parent class and reference of a child class.
+//📌 Golden Rule:
+//👉 You can always upcast (child → parent reference).
+//👉 You cannot directly downcast (parent → child reference) unless you explicitly cast and the object really is a child
 
-interface Employee{
-    //These variables are final and static:
-    int salary=12000;
-    String company = "Mckinsey";
+//Rule: A child reference cannot point to a parent object.
+//Reason: A parent object does not have the extra things the child adds.
+//Example: A doesn’t know about bShow(). If you allowed B b = new A();, then calling b.bShow() would be impossible because the actual object is only A (no such method exists in memory).
 
-   void code();
-   void manage();
-
-}
-
-class CEO implements Employee{
-    public void code(){
-        System.out.println("Inside code method defined inside CEO class");
-    }
-    public void manage(){
-        System.out.println("Inside manage method defined inside CEO class");
-    }
-    //Separate method for CEO:
-    public void testing(){
-        System.out.println("Inside testing method defined inside CEO class which is not implementing Employee class methods");
-
+class A{
+    public  void aShow(){
+        System.out.println("In aShow");
     }
 }
 
-class Hello{
+class B extends A{
+    public void bShow(){
+        System.out.println("In bShow");
+    }
+}
+
+public class Hello{
     public static void main(String []args){
-        System.out.println("inside main function");
-        //Object of child class and reference of parent class .
-        Employee e = new CEO(); //Creating reference of Employee and object of CEO class
-        e.code();
-        e.manage();
-        System.out.println();
-        CEO c = new CEO();
-        c.code();
-        c.manage();
-        c.testing();
+        System.out.println("Inside main function");
+        A a = new B();
+        a.aShow();
+        
     }
 }
