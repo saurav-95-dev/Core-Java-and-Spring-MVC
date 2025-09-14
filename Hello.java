105847d1c55b467dee47246ec59264ec2cfb9ba5
Saurabh <devsaurabh086@gmail.com>
2025-09-15 00:53:12 +0530

    Need for an interface-tight-coupling-2

--- Hello.java
+++ Hello.java
@@ -1,13 +1,17 @@
 //Need for the interface :
 //This is an example of tight coupling:
 
-class Laptop{
+abstract class Computer{
+   public abstract void code();
+}
+
+class Laptop extends Computer{
     public void code(){
         System.out.println("Developing application using laptop");
     }
 }
 
-class Desktop{
+class Desktop extends  Computer{
     public void code(){
         System.out.println("Developing application using desktop");
     }
@@ -15,18 +19,18 @@ class Desktop{
 
 class Developer{
     //developApplication method is dependent on device specific object
-    public void developApplication(Desktop d){
+    public void developApplication(Computer device){
         System.out.println("Inside developApplication method..");
-        d.code();
+        device.code();
     }
 }
 
 public class Hello{
     public static void main(String[] args) {
         System.out.println("Inside main function");
-        Laptop l = new Laptop();
-        Desktop d = new Desktop();
+        Computer desktop = new Desktop();
+        Computer laptop = new Laptop();
         Developer saurabh = new Developer();
-        saurabh.developApplication(d);
+        saurabh.developApplication(laptop);
     }
 }
