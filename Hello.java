813d49581099f73b350013d6629c0c9a4d162ee9
Saurabh <devsaurabh086@gmail.com>
2025-09-15 00:44:58 +0530

    Need for an interface-tight-coupling

--- Hello.java
+++ Hello.java
@@ -1,13 +1,14 @@
 //Need for the interface :
 //This is an example of tight coupling:
+
 class Laptop{
-    public void laptop(){
+    public void code(){
         System.out.println("Developing application using laptop");
     }
 }
 
 class Desktop{
-    public void desktop(){
+    public void code(){
         System.out.println("Developing application using desktop");
     }
 }
@@ -16,7 +17,7 @@ class Developer{
     //developApplication method is dependent on device specific object
     public void developApplication(Desktop d){
         System.out.println("Inside developApplication method..");
-        d.desktop();
+        d.code();
     }
 }
 
