b97ded75d056095f7505a83c663ac793ef3c7b86
Saurabh <devsaurabh086@gmail.com>
2025-09-16 23:42:23 +0530

    Can static method be overriden-02

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 //Can we override a static method :
-
+//NOTES:
+//We cannot override a static method because overriding is achieved by runtime polymorphism in which method calling depends on the object type .
+//But static methods don't belong to object , rather they belong to class , hence the method call will be decided at compile time and will depend on the reference type.
 class A{
     public static void code(){
         System.out.println("Code inside class A");
@@ -22,8 +24,12 @@ class Hello{
     public static void main(String[] args) {
 
         B b = new B();
-        b.manage(); //Will call manage method under B subclass
+        b.manage(); //Will call manage method under B subclass since its non-static .
         b.code(); //Will call code method under B subclass (although discouraged to call static method via object).
+        System.out.println();
+        A a = new B();
+        a.manage(); //manage method was overriden because it was non-static method.
+        a.code(); //code is not overriden as it was static and hence method call depends on reference type here .
 
     }
 }
