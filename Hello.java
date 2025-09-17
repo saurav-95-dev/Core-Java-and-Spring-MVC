a53d0923f8c70f0990e34fb412094e5b2d9fe103
Saurabh <devsaurabh086@gmail.com>
2025-09-17 21:39:45 +0530

    Enums-01

--- Hello.java
+++ Hello.java
@@ -1,34 +1,17 @@
-//Abstract classes can have constructors but interfaces cannot , why ?
-//Abstract classes :
+//Enums in java :
+//NOTES :
+//Enums are named constant.
 
-abstract class A{
-    int salary;
-    String company;
-
-    public A(){
-        System.out.println("A's constructor called..");
-    }
-
-   public abstract void code();
-   public abstract void manage();
-}
-
-class B extends A{
-
-    public B(){
-        System.out.println("B's constructor called..");
-    }
-    public void code(){
-        System.out.println("code is implemented");
-    }
-    public void manage(){
-        System.out.println("manage is implemented");
-    }
+enum Status{
+    //Now here , as per java, Status is a class .
+    Running , Failed , Success , Pending; //These are the objects name.
 }
 
 class Hello{
-    public static void main(String [] args){
 
-        A a = new B();
+    public static void main(String [] args){
+         Status s = Status.Success;
+        System.out.println(s);
+
     }
 }
