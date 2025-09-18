3b8f43adea507be8366a2695b91d32fd133bc135
Saurabh <devsaurabh086@gmail.com>
2025-09-18 19:08:47 +0530

    Enum-REV-01

--- Hello.java
+++ Hello.java
@@ -2,41 +2,27 @@
 //NOTES :
 //Enums are named constant.
 //Switch is recommended in case of Enums rather than if-else.
+//Although enums are treated as class in java , but you cannot extend enums with any other class.
+//Apart from the above you can have objects , methods , variables as well as constructors.
+//By-default enum in java extends the enum class.
 
-
-enum Status{
-    //Now here , as per java, Status is a class .
-    Running , Failed , Success , Pending; //These are the objects name.
+enum Status {
+    waiting , success , error , failed , pending;
 }
 
 class Hello{
-
     public static void main(String [] args){
-         Status s = Status.Success;
-        System.out.println(s.ordinal() + "->" + s);
+        System.out.println("Inside main function");
+        Status s = Status.failed;
+        System.out.println(s);
+        System.out.println("Ordinal of " + s + " is " + s.ordinal());
         System.out.println();
-        //To print all the status :
-        Status []arr = Status.values();
-        for( Status i : arr){
-            System.out.println(i + "->" + i.ordinal());
+        System.out.println("Printing all values : ");
+        //printing all values :
+        Status[] arr = Status.values();
+        for(Status i : arr){
+            System.out.println(i + "->" + i.ordinal() );
         }
-        System.out.println();
-        Status s1 = Status.Failed;
-        switch (s1){
-            case Running :
-                System.out.println("Running...");
-                break;
-            case Failed :
-                System.out.println("Failed...");
-                break;
-            case Success:
-                System.out.println("Success ...");
-                break;
-            default:
-                System.out.println("Pending...");
-                break;
-        }
-
 
     }
 }
