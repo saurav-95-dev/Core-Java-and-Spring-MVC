f7204ff90a4375629a39b6438d6cd963f1806ef6
Saurabh <devsaurabh086@gmail.com>
2025-09-18 23:05:32 +0530

    Abstract classes can have constuctors-demo-01

--- Hello.java
+++ Hello.java
@@ -1,79 +1,44 @@
-//Abstraction with anonymous inner class :
-// You have very very less time.
-//You have no time to depress neither you have the time to take rest
+//interface cannot have constructors but abstract classes can :
 
 abstract class A{
+
     public abstract void code();
     public abstract void manage();
-    public void testing(){
-        System.out.println("testing inside class A");
-    }
-    public void Test(){
-        System.out.println("Test of parent class");
-    }
 
-    public void demo(){
-        System.out.println("Demo inside class A");
+    public int salary;
+    public String company;
+
+    public A(int salary , String company){
+        System.out.println("Parent constructor called");
+        this.salary = salary;
+        this.company = company;
     }
 
 }
 
 class B extends A{
-    public void code(){
-        System.out.println("Implemented code function");
+
+    public void code() {
+        System.out.println("code implemented inside B class");
     }
-    public void manage(){
-        System.out.println("Implemented manage function");
+
+    public void manage() {
+        System.out.println("manage implemented inside B class");
     }
-    public void analyst(){
-        System.out.println("Inside analyst under subclass class B");
-    }
-    public void Test(){
-        System.out.println("Test of child class");
+
+    public B(int salary , String company){
+
+        super(salary ,company);
+        this.salary = salary;
+        this.company = company;
+        System.out.println("Child constructor called");
     }
 }
 
 class Hello{
-    public static void main(String [] args){
-        A a = new B()
-        {
+    public static void main(String []args){
 
-            public void demo() {
-                System.out.println("Demo is changed");
-            }
-        };
-        a.manage();
-        a.code();
-        a.testing();
-        a.Test(); // Method overriding is on action
-        System.out.println();
-        System.out.println("Making reference of B now -------------------");
-        B b = new B();
-        b.manage();
-        b.analyst();
-        b.code();
-        b.Test();
-        a.demo();
-
-        A a1 = new A()
-        {
-            public void code(){
-                System.out.println("implementation of code changed");
-            }
-            public void manage(){
-                System.out.println("implementation of manage changed");
-            }
-
-        };
-        System.out.println();
-        System.out.println("After changing the implementation of code and manage:");
-        a1.code();
-        a1.manage();
-
-
-
-
-
-
+        A a = new B(123 , "DXC");
+        System.out.println(a.salary  + " , " + a.company);
     }
 }
