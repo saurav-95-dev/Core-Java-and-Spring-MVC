2fd6feae0084993bf2b69e4f702e2871f75d9b50
Saurabh <devsaurabh086@gmail.com>
2025-09-18 23:24:01 +0530

    Static method cannot be overridden

--- Hello.java
+++ Hello.java
@@ -1,44 +1,32 @@
-//interface cannot have constructors but abstract classes can :
+//Can we ovveride static method ?
 
-abstract class A{
-
-    public abstract void code();
-    public abstract void manage();
-
-    public int salary;
-    public String company;
-
-    public A(int salary , String company){
-        System.out.println("Parent constructor called");
-        this.salary = salary;
-        this.company = company;
+class A{
+    public void code(){
+        System.out.println("code inside a");
     }
-
+    public static void manage(){
+        System.out.println("manage inside a");
+    }
 }
 
 class B extends A{
-
-    public void code() {
-        System.out.println("code implemented inside B class");
+    public void code(){
+        System.out.println("code method changed via child");
     }
 
-    public void manage() {
-        System.out.println("manage implemented inside B class");
-    }
-
-    public B(int salary , String company){
-
-        super(salary ,company);
-        this.salary = salary;
-        this.company = company;
-        System.out.println("Child constructor called");
+    public static void manage(){
+        System.out.println("static manage method overriden");
     }
 }
 
 class Hello{
-    public static void main(String []args){
-
-        A a = new B(123 , "DXC");
-        System.out.println(a.salary  + " , " + a.company);
+    public static void main(String [] args) {
+            A a = new B();
+            a.code();
+            a.manage();//static method was not overriden , method call depends on reference type which is A here
+        System.out.println();
+        B b = new B();
+        b.code();
+        b.manage();
     }
 }
