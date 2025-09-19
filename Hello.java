4b4f9b53806289b8040d559eb81352be0943730e
Saurabh <devsaurabh086@gmail.com>
2025-09-19 21:24:15 +0530

    Annotations-01

--- Hello.java
+++ Hello.java
@@ -1,32 +1,25 @@
-//Can we ovveride static method ?
+//Annotations and Types of Interface :
+//Annotations are meant for resolving the bugs at run time.
 
 class A{
     public void code(){
-        System.out.println("code inside a");
-    }
-    public static void manage(){
-        System.out.println("manage inside a");
+        System.out.println("coding in a class ");
     }
 }
 
 class B extends A{
-    public void code(){
-        System.out.println("code method changed via child");
-    }
 
-    public static void manage(){
-        System.out.println("static manage method overriden");
+    @Override
+    public void code(){
+        System.out.println("coding in b class");
     }
 }
 
 class Hello{
-    public static void main(String [] args) {
-            A a = new B();
-            a.code();
-            a.manage();//static method was not overriden , method call depends on reference type which is A here
-        System.out.println();
-        B b = new B();
-        b.code();
-        b.manage();
+
+    public static void main(String [] args){
+        A a = new B();
+        a.code();
     }
+
 }
