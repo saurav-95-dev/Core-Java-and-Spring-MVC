2753213b7ecffe89301d77e2537c334a7bd5bf64
Saurabh <devsaurabh086@gmail.com>
2025-09-20 23:25:52 +0530

    new change01

--- Hello.java
+++ Hello.java
@@ -5,41 +5,5 @@
 //Marker interfaces are actually blank interface with no method.
 //**We can use lambda expression only with functional interface.
 
-@FunctionalInterface //Annotations used here
-interface A{
-    void code(int i);
-}
 
-interface B{
-    int manage(int a , int b);
-}
-
-interface C{
-    public String testing(String name);
-}
-
-class Hello{
-
-    public static void main(String [] args){
-       A a = (int i)->{
-           System.out.println("code implemented in lamda expression-" + i);
-       };
-
-       a.code(123);
-
-       B b = (int x , int y)->{
-           return x+y;
-       };
-       int result  = b.manage(12,4);
-        System.out.println(result);
-
-        C c = (String name) ->{
-            return name;
-        };
-
-        String str = c.testing("Sapient");
-        System.out.println(str);
-    }
-
-}
 
