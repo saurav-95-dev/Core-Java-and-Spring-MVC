a6ba411b5d0e37502143b0ba068644e183b099e7
Saurabh <devsaurabh086@gmail.com>
2025-09-21 20:53:00 +0530

    throwing exceptions via 'throw new'

--- Hello.java
+++ Hello.java
@@ -2,31 +2,29 @@
 //throw keyword is used to throw the exception for the catch block to catch it .
 //If we are doing a valid arithmetic for example , catch block won't get executed .
 //Here we want to catch block to be executed for even a valid arithmetic operation , for that purpose we can use throw.
-//
+//throwing self created custom exception.
 
 class Hello{
-    public static void main() {
-        int i= 12;
+    public static void main(String [] args){
+        int i = 0;
         int j = 2;
         int result = 0;
 
         try{
             result = i/j;
-            if(j==2){
-                throw new ArithmeticException("This is a custom message alongwith the default one");
+            if(result == 0){
+                throw new ArithmeticException("Additional message here..");
             }
+
         }
         catch(ArithmeticException e){
-            result = i / 6;
-            System.out.println("Something went wrong here...!" + e);
+            System.out.println("Something went wrong..!" + e);
         }
-        catch(Exception e){
-            System.out.println("Inside parent exception class" + e);
-        }
-        System.out.println("Inside main function");
 
+        System.out.println("Inside main function...");
         System.out.println();
         System.out.println(result);
+        System.out.println();
         System.out.println("Last statement inside main function..");
     }
 }
