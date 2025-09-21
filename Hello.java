e448f5613d39f66d5d175d717c4eab6e1f0efba4
Saurabh <devsaurabh086@gmail.com>
2025-09-21 16:30:05 +0530

    Throws-keyword-01

--- Hello.java
+++ Hello.java
@@ -1,36 +1,32 @@
-//Exceptions in java :
-//Types of error -> Compile time error , Runtime errors , logical errors .
-//Exceptions are basically run-time errors .
-//We basically tend to handle the exceptions .
-//It is recommended to put only critical statement inside try block , if it runs fines , control will skip the catch block.
-//Control only executes the catch block if something goes wrong inside try.
-//Exception is like a parent class of all type of exceptions.
-//Class hierarchy in the context of exceptions ->
-//object class --> Throwable class --> Errors (can't handle) & Exceptions(can/should be handled).
-//Errors --> can be ThreadDeath , VM Error  , IO error , outOfMemory error .
-//Exceptions --> can be Runtime (Arithmetic , arrayIndexOutOfBound , Null pointer) which are checked exceptions or else it can be SQL Exceptions/IO Exceptions etc. which are basically checked exceptions.
-//In case of runtime exception , there ain't a compulsion to handle the exception , but in case of an exception which is not run-time exception , compiler will force you to handle the exception.
-
+//Throw/throws keyword:
+//throw keyword is used to throw the exception for the catch block to catch it .
+//If we are doing a valid arithmetic for example , catch block won't get executed .
+//Here we want to catch block to be executed for even a valid arithmetic operation , for that purpose we can use throw.
+//
 
 class Hello{
-    public static void main(String []args){
-        int i = 12;
-        int j = 0;
+    public static void main() {
+        int i= 12;
+        int j = 2;
         int result = 0;
 
         try{
-            result = i/j; //Run time error
+            result = i/j;
+            if(j==2){
+                throw new ArithmeticException();
+            }
         }
-        catch (ArithmeticException e){
-            System.out.println("Here is the exception -> " + e);
+        catch(ArithmeticException e){
+            result = i / 6;
+            System.out.println("Something went wrong here...!");
         }
-        catch (Exception e){
-            System.out.println("Something went wrong here..!");
+        catch(Exception e){
+            System.out.println("Inside parent exception class" + e);
         }
-
         System.out.println("Inside main function");
+
+        System.out.println();
         System.out.println(result);
-        System.out.println("last statement");
+        System.out.println("Last statement inside main function..");
     }
 }
-
