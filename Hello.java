0f1e184ec377c75bd63a21956a498eb3ac4163e0
Saurabh <devsaurabh086@gmail.com>
2025-09-22 21:22:44 +0530

    finally block intro

--- Hello.java
+++ Hello.java
@@ -1,19 +1,28 @@
 //Taking user-input in java :
-//NOTE --> Interview ques -> println belongs to which class ? -->It belongs to print-stream class
-//We can take input via bufferReader or by scanner . In case of bufferReader , it's a resource , hence it is your responsibility to close it
-//BufferReader can not only take input from key , but also from file , network etc.
+
+//Implementing try with finally/ try with resources .
+//try can be used with finally.
+//finally is used where we want to execute some statement irrespective of the exception that we encounter.
+//finally can be used with try in order to close a resource.
+//finally is basically meant for closing the resource, closing DB connection , closing file etc.
 
 
+import javax.xml.transform.Source;
 import java.util.Scanner;
 
 class Hello{
     public static void main(String [] args){
 
-        System.out.println("Inside main function");
-        System.out.println("Enter a number");
-        Scanner sc = new Scanner(System.in);
-        int result = sc.nextInt();
-        System.out.println(result);
+        System.out.println("Inside main");
+        Scanner sc = null;
+        try{
+            sc = new Scanner(System.in);
+            int result = sc.nextInt();
+            System.out.println(result);
+        }
+        finally {
+           sc.close();
+        }
 
     }
 }
