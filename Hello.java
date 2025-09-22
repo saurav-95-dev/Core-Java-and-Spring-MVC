1ed01a48beb45874ada54e62d5ccef5f9e6615cc
Saurabh <devsaurabh086@gmail.com>
2025-09-22 21:10:31 +0530

    input via scanner class

--- Hello.java
+++ Hello.java
@@ -1,31 +1,19 @@
-//Working of throws keyword :
-//NOTES:
-//throws is used when there is ducking of the exceptions , and it makes sense for checked exceptions.
+//Taking user-input in java :
+//NOTE --> Interview ques -> println belongs to which class ? -->It belongs to print-stream class
+//We can take input via bufferReader or by scanner . In case of bufferReader , it's a resource , hence it is your responsibility to close it
+//BufferReader can not only take input from key , but also from file , network etc.
 
-class A{
-    public void show() throws ClassNotFoundException{
 
-        System.out.println("Inside show function");
-
-            Class.forName("Demo");
-
-    }
-}
+import java.util.Scanner;
 
 class Hello{
-
-    static {
-        System.out.println("Main class is loaded...");
-    }
     public static void main(String [] args){
 
         System.out.println("Inside main function");
-        A a = new A();
-        try{
-            a.show();
-        }
-        catch (ClassNotFoundException e){
-            System.out.println("OOPs .. .class not found.." + e);
-        }
+        System.out.println("Enter a number");
+        Scanner sc = new Scanner(System.in);
+        int result = sc.nextInt();
+        System.out.println(result);
+
     }
 }
