0d06ae5c351f62a97b15ee66a72888c1f05f42be
Saurabh <devsaurabh086@gmail.com>
2025-09-23 13:19:01 +0530

    creating thread via implementing runnable interface

--- Hello.java
+++ Hello.java
@@ -1,15 +1,10 @@
-//Two main ways of creating a thread --> By extending a thread class , by implementing the runnable interface .
-//METHOD -1 : By extending a Thread class :
-//In this approach we directly create a class that is inherited from Thread class and overrides its run() method.
+//Creating thread via implementing runnable interface :
+//Thread is basically a class which implements runnable interface . Inside runnable interface , we have a method called run.
+//In runnable interface we don't have start method , previously it was coming from Thread class.
 
-//THREAD PRIORITY :
-//-> Multiple Threads and Thread priority adn sleep .
-//-> getPriority method --> gives you the current priority of the thread.
-//-> Range of this priority goes from 1 to 10.
-
-class A extends Thread{
+class A implements Runnable{
      public void run(){
-         for(int i=1;i<100;i++){
+         for(int i=1;i<5;i++){
              System.out.println("in a");
              try {
                  Thread.sleep(10); //try-catch because sleep will throw exception
@@ -20,9 +15,9 @@ class A extends Thread{
      }
 }
 
-class B extends Thread{
+class B implements Runnable{
     public void run(){
-        for(int i=1;i<100;i++){
+        for(int i=1;i<5;i++){
             System.out.println("in b");
             try {
                 Thread.sleep(10); //try-catch because sleep will throw exception
@@ -36,14 +31,17 @@ class B extends Thread{
 class Hello{
     public static void main(String [] args){
 
-        A a = new A();
-        System.out.println(a.getPriority());
-        a.setPriority(6);
-        a.start();
+//        A a = new A();
+//        B b = new B();
+        Runnable a = new A(); //we can create reference of an interface and object of the class.
+        Runnable b = new B();
 
-        B b = new B();
-        b.setPriority(Thread.MAX_PRIORITY);
-        b.start();
+        Thread t1 = new Thread(a);
+        Thread t2 = new Thread(b);
+        t1.start();
+        t2.start();
+//        a.start();
+//        b.start();
 
     }
 }
