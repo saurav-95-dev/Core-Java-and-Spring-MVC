ab7b0d473ebffc22af67192c30558abbee5ba30b
Saurabh <devsaurabh086@gmail.com>
2025-09-23 12:57:38 +0530

    creating thread via extending Thread class

--- Hello.java
+++ Hello.java
@@ -1,28 +1,32 @@
-//Multiple Threads and Thread priority adn sleep .
-//getPriority method --> gives you the current priority of the thread.
-//Range of this priority goes from 1 to 10.
+//Two main ways of creating a thread --> By extending a thread class , by implementing the runnable interface .
 
-class  A extends Thread{
-    public void run(){
-        for(int i=1;i<100;i++){
-            System.out.println("in a run");
-        }
-    }
+//METHOD -1 : By extending a Thread class :
+//In this approach we directly create a class that is inherited from Thread class and overrides its run() method.
+
+
+class A extends Thread{
+     public void run(){
+         for(int i=1;i<100;i++){
+             System.out.println("in a");
+         }
+     }
 }
 
 class B extends Thread{
     public void run(){
         for(int i=1;i<100;i++){
-            System.out.println("in b run");
+            System.out.println("in b");
         }
     }
 }
 
 class Hello{
-    public static void main(String [] args ){
+    public static void main(String [] args){
+
         A a = new A();
-        a.start(); //Directly calling run method will execute the method as normal method call and thus no new thread will be created.
+        a.start();
         B b = new B();
         b.start();
+
     }
 }
