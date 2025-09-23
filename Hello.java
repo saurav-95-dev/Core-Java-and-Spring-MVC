8635ac2ae3b0a3fc1d8a8ae2f4334c097c079ce8
Saurabh <devsaurabh086@gmail.com>
2025-09-23 20:11:22 +0530

    Exception REV-01

--- Hello.java
+++ Hello.java
@@ -1,47 +1,35 @@
-//Creating thread via implementing runnable interface :
-//Thread is basically a class which implements runnable interface . Inside runnable interface , we have a method called run.
-//In runnable interface we don't have start method , previously it was coming from Thread class.
+//Exception REV-01:
 
-class A implements Runnable{
-     public void run(){
-         for(int i=1;i<5;i++){
-             System.out.println("in a");
-             try {
-                 Thread.sleep(10); //try-catch because sleep will throw exception
-             } catch (InterruptedException e) {
-                 throw new RuntimeException(e);
-             }
-         }
-     }
-}
-
-class B implements Runnable{
-    public void run(){
-        for(int i=1;i<5;i++){
-            System.out.println("in b");
-            try {
-                Thread.sleep(10); //try-catch because sleep will throw exception
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
+class SaurabhException extends Exception{
+    public SaurabhException(String s){
+        super(s);
     }
 }
 
 class Hello{
     public static void main(String [] args){
+        int i=12;
+        int j=2;
+        int result = 0;
+        try{
+            result = i/j;
+            if(result == 2){
+                throw new ArithmeticException("This is additional message as the result is 2");
+            }
 
-//        A a = new A();
-//        B b = new B();
-        Runnable a = new A(); //we can create reference of an interface and object of the class.
-        Runnable b = new B();
+            if(result == 6){
+                throw new SaurabhException("This is additional message from SaurabhException as the result is 6");
+            }
+        }
+        catch(ArithmeticException e){
+            System.out.println("Something went wrong because the result is probably 2 or denominator is 0 ->" + e);
+        }
+        catch (SaurabhException e){
+            System.out.println("Something went wrong because the result is probably 6->" + e);
+        }
 
-        Thread t1 = new Thread(a);
-        Thread t2 = new Thread(b);
-        t1.start();
-        t2.start();
-//        a.start();
-//        b.start();
+        System.out.println(result);
+        System.out.println("last statement of main...");
 
     }
 }
