baacfa0a1f03998f3bf59a4d8eb3b5cbab4ef5a7
Saurabh <devsaurabh086@gmail.com>
2025-09-23 20:35:33 +0530

    Exception REV-02

--- Hello.java
+++ Hello.java
@@ -1,35 +1,24 @@
-//Exception REV-01:
+//throws : --> used where there  is ducking of exceptions:
 
-class SaurabhException extends Exception{
-    public SaurabhException(String s){
-        super(s);
-    }
+class A {
+   public void show() throws ClassNotFoundException{
+       System.out.println("in a show ");
+
+           Class.forName("Demo");
+
+   }
 }
 
 class Hello{
+
     public static void main(String [] args){
-        int i=12;
-        int j=2;
-        int result = 0;
-        try{
-            result = i/j;
-            if(result == 2){
-                throw new ArithmeticException("This is additional message as the result is 2");
-            }
-
-            if(result == 6){
-                throw new SaurabhException("This is additional message from SaurabhException as the result is 6");
-            }
-        }
-        catch(ArithmeticException e){
-            System.out.println("Something went wrong because the result is probably 2 or denominator is 0 ->" + e);
-        }
-        catch (SaurabhException e){
-            System.out.println("Something went wrong because the result is probably 6->" + e);
-        }
-
-        System.out.println(result);
-        System.out.println("last statement of main...");
+         A a  = new A();
+         try{
+             a.show();
+         }
+         catch(ClassNotFoundException e){
+             System.out.println("Ooops , ....class not found !");
+         }
 
     }
 }
