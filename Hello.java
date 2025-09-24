857476460600eb223562fbab449d5759df7d7b2b
Saurabh <devsaurabh086@gmail.com>
2025-09-25 02:49:13 +0530

    preventing mutiple threads to access the same method via synchronized

--- Hello.java
+++ Hello.java
@@ -1,15 +1,19 @@
 //Multi-Threading :
 //Race conditions : When 2 or more threads try to access the same method to achieve same operation .
+//Preventing race condition :
+//join() method --> it allows main thread to wait for other 2 threads to come back and join.
+
 
 class A{
     int count;
-    public void increment(){
+    // here we are using synchronized to basically stop both the threads from accessing the increment method.
+    public synchronized void increment(){
         count++;
     }
 }
 
 class Hello{
-    public static void main(String [] args) {
+    public static void main(String [] args) throws InterruptedException {
         A a1 = new A();
         //Thread 1:
         Runnable a = ()->{
@@ -27,7 +31,8 @@ class Hello{
         Thread t2 = new Thread(b);
         t1.start();
         t2.start();
-
+        t1.join();
+        t2.join();
         System.out.println(a1.count);
 
     }
