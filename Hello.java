285ccaf97bccda9cad54c1475f87e10ba32a28ff
Saurabh <devsaurabh086@gmail.com>
2025-09-25 01:38:53 +0530

    Creating thread by implementing runnable interface via lamda expression.

--- Hello.java
+++ Hello.java
@@ -1,25 +1,21 @@
 //Multi-Threading :
-//Creating thread by implementing runnable interface via anonymous inner class.
+//Creating thread by implementing runnable interface via lamda expression.
 
 
 class Hello {
     public static void main(String[] args) {
         System.out.println("Inside main function");
 
-        Runnable a = new Runnable(){
-            public void run(){
-                for(int i=1;i<100;i++){
-                    System.out.println("Hii");
-                }
+        Runnable a = ()->{
+            for(int i=1;i<100;i++){
+                System.out.println("Hii");
             }
         };
         Thread t1 = new Thread(a);
         t1.start();
-        Runnable b = new Runnable() {
-            public void run() {
-                    for(int i=1;i<100;i++){
-                        System.out.println("Hello");
-                    }
+        Runnable b =()->{
+            for(int i=1;i<100;i++){
+                System.out.println("Hello");
             }
         };
         Thread t2 = new Thread(b);
