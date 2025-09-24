2256771d86e06ee480625bc3ef2a9df6f8a41118
Saurabh <devsaurabh086@gmail.com>
2025-09-25 01:35:37 +0530

    Creating thread by implementing runnable interface via anonymous inner class

--- Hello.java
+++ Hello.java
@@ -1,31 +1,27 @@
 //Multi-Threading :
 //Creating thread by implementing runnable interface via anonymous inner class.
 
-class A implements Runnable{
-    public void run(){
-        for(int i=1;i<100;i++){
-            System.out.println("hii");
-        }
-    }
-}
-
-class B implements Runnable{
-    public void run(){
-        for(int i=1;i<100;i++){
-            System.out.println("hello");
-        }
-    }
-}
-
 
 class Hello {
     public static void main(String[] args) {
         System.out.println("Inside main function");
 
-        Runnable a = new A();
+        Runnable a = new Runnable(){
+            public void run(){
+                for(int i=1;i<100;i++){
+                    System.out.println("Hii");
+                }
+            }
+        };
         Thread t1 = new Thread(a);
         t1.start();
-        Runnable b = new B();
+        Runnable b = new Runnable() {
+            public void run() {
+                    for(int i=1;i<100;i++){
+                        System.out.println("Hello");
+                    }
+            }
+        };
         Thread t2 = new Thread(b);
         t2.start();
 
