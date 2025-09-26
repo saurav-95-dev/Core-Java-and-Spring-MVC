55f77da5b48614e67c3fb7efa5d4d6ae7e02a5be
Saurabh <devsaurabh086@gmail.com>
2025-09-26 23:18:10 +0530

    new change

--- Hello.java
+++ Hello.java
@@ -1,39 +1,42 @@
-//Multi-Threading :
-//Race conditions : When 2 or more threads try to access the same method to achieve same operation .
-//Preventing race condition :
-//join() method --> it allows main thread to wait for other 2 threads to come back and join.
+//Threads-REV :
 
+class A extends Thread{
+    public void run(){
+        for(int i=1;i<100;i++){
+            System.out.println("Hii..");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
 
-class A{
-    int count;
-    // here we are using synchronized to basically stop both the threads from accessing the increment method.
-    public synchronized void increment(){
-        count++;
+class B extends Thread{
+    public void run(){
+        for(int i=1;i<100;i++){
+            System.out.println("Hello..");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
 
 class Hello{
-    public static void main(String [] args) throws InterruptedException {
-        A a1 = new A();
-        //Thread 1:
-        Runnable a = ()->{
-            for(int i=1;i<=1000;i++){
-                a1.increment();
-            }
-        };
-        //Thread 2:
-        Runnable b =()->{
-            for(int i=1;i<=1000;i++){
-                a1.increment();
-            }
-        };
-        Thread t1 = new Thread(a);
-        Thread t2 = new Thread(b);
-        t1.start();
-        t2.start();
-        t1.join();
-        t2.join();
-        System.out.println(a1.count);
+    public static void main(String []args){
+        A a = new A();
+        B b = new B();
+        System.out.println(a.getPriority());
+        System.out.println(b.getPriority());
+        a.start();
+
+        a.setPriority(1);
+        b.start();
+        b.setPriority(Thread.MAX_PRIORITY);
 
     }
 }
