596c9e55a13fcb12c7450e29d6b7df8f19e991f0
Saurabh <devsaurabh086@gmail.com>
2025-10-03 23:28:26 +0530

    Collection changes-01 error fixed

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class ListComparison {
+class ListComparison {
     public static void main(String[] args) {
         // --- ArrayList vs. LinkedList performance ---
         int size = 100_000;
