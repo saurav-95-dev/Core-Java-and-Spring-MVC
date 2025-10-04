7e5ab3f55a77491aab65c3f6d074bdd8870366cf
Saurabh <devsaurabh086@gmail.com>
2025-10-04 19:32:48 +0530

    Printing set directly and via loop

--- Hello.java
+++ Hello.java
@@ -3,9 +3,7 @@
 //Collection belongs to java.util package.
 //Collections work with objects and not with the primitive values.
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 class Hello{
     public static void main(String [] args){
@@ -29,12 +27,25 @@ class Hello{
         l.add(3);
         l.add(4);
         l.add(5);
+        l.add(3);
         System.out.println("Printing list directly:");
         System.out.println(l);
         System.out.println("printing list using for loop");
         for (int n : l){
             System.out.println(n);
         }
+        System.out.println("Printing values using set");
 
+        Set<Integer> s = new HashSet<Integer>();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        s.add(4);
+        System.out.println("Printing set values directly:");
+        System.out.println(s);
+        System.out.println("Printing set values using loop:");
+        for (int i : s){
+            System.out.println(i);
+        }
     }
 }
