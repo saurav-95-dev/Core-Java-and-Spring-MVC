d6b4b1eb293e662b7c6132b7acc444b0e22b84d5
Saurabh <devsaurabh086@gmail.com>
2025-10-04 19:18:58 +0530

    Printing collection using loop with object type

--- Hello.java
+++ Hello.java
@@ -1,8 +1,7 @@
 //Starting Collections :
 //->Starting with collection interface:
 //Collection belongs to java.util package.
-//Collections work with objects.
-
+//Collections work with objects and not with the primitive values.
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -14,6 +13,13 @@ class Hello{
         nums.add(2);
         nums.add(3);
         nums.add(4);
+        nums.add(5);
+        nums.add("6"); //Here it will give run time exception.Hence, to prevent this , we use generics.
+        System.out.println("Printing the values directly:");
         System.out.println(nums);
+        System.out.println("Printing using for loop:");
+        for (Object n : nums){
+            System.out.println((int)n);
+        }
     }
 }
