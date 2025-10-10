decc099b7e96920780215774cc160af834b5165c
Saurabh <devsaurabh086@gmail.com>
2025-10-10 15:49:55 +0530

    Collection-REV02

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 class Hello{
     public static void main(String[]args){
@@ -12,8 +13,14 @@ class Hello{
         c.add(1);
         c.add(2);
         c.add(3);
+        c.add("3");
         System.out.println("Printing collection directly");
         System.out.println(c);
+        System.out.println("printing via loop without generics:");
+        for(Object o : c){
+            int n = (int)o;
+            System.out.println(n);
+        }
     }
 }
 
