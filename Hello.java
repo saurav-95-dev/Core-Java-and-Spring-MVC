b808645e19059e688d2aacfd853398d34f87b3a0
Saurabh <devsaurabh086@gmail.com>
2025-10-10 15:53:51 +0530

    REV-Printing via loop using generics

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 //Java Collection-REV-01 :
 //When we talk about collection-api we are talking about concept ,when we talk about Collections , it is about interface
 //and when we talk about Collections , we are talking about class.
+//Collection works with objects and not with primitive type.
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -9,11 +10,10 @@ import java.util.Objects;
 class Hello{
     public static void main(String[]args){
         System.out.println("Working with collection:");
-        Collection c = new ArrayList();
+        ArrayList<Integer> c = new ArrayList<>();
         c.add(1);
         c.add(2);
         c.add(3);
-        c.add("3");
         System.out.println("Printing collection directly");
         System.out.println(c);
         System.out.println("printing via loop without generics:");
@@ -21,6 +21,11 @@ class Hello{
             int n = (int)o;
             System.out.println(n);
         }
+        System.out.println("printing via loop using generics:");
+        for(int i : c){
+            System.out.println(i);
+        }
+
     }
 }
 
