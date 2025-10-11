93c8ce3bfa2eb57fb19c4c8180069bb9e54f7396
Saurabh <devsaurabh086@gmail.com>
2025-10-11 20:00:59 +0530

    collection behaviour without generics

--- Hello.java
+++ Hello.java
@@ -1,69 +1,26 @@
-//Java Collection-REV-01 :
-//When we talk about collection-api we are talking about concept ,when we talk about Collections , it is about interface
-//and when we talk about Collections , we are talking about class.
-//Collection works with objects and not with primitive type.
+//Collection-REV-02:
+//Collection is an interface , Collections is a class and Collection api is a concept.
+//Iterable interface extends Collection interface .
+//List , Queue and Set are 3 interfaces that are extended by Collection interface.
+//List is implemented by class called ArrayList , and Set is implemented by class called HashSet().
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
 class Hello{
     public static void main(String[]args){
         System.out.println("Working with collection:");
-        ArrayList<Integer> c = new ArrayList<>();
+        Collection c = new ArrayList();
         c.add(1);
         c.add(2);
         c.add(3);
-        System.out.println("Printing collection directly");
+        c.add(4);
+        c.add("5");
+        System.out.println("printing collection directly without generics");
         System.out.println(c);
-        System.out.println("printing via loop without generics:");
+        System.out.println("printing collection via loop without using generics");
         for(Object o : c){
-            int n = (int)o;
-            System.out.println(n);
+            System.out.println(o);
         }
-        System.out.println("printing via loop using generics:");
-        for(int i : c){
-            System.out.println(i);
-        }
-        System.out.println("Trying get method:");
-        System.out.println(c.get(0));;
-        System.out.println();
-        System.out.println("Working with list:");
-        List<Integer> l = new ArrayList<Integer>();
-        l.add(11);
-        l.add(12);
-        l.add(13);
-        l.add(14);
-        l.add(12);
-        l.add(15);
-        System.out.println("printing directly:");
-        System.out.println(l);
-        for(int i : l){
-            System.out.println(i);
-        }
-        System.out.println("index of method:");
-        System.out.println(l.indexOf(12));
-        System.out.println("Working with set:");
-        Set<Integer> s = new HashSet<>();
-        s.add(22);
-        s.add(23);
-        s.add(24);
-        s.add(25);
-        s.add(23);
-        s.add(22);
-        s.add(1);
-        s.add(2);
-        System.out.println("printing directly:");
-        System.out.println(s);
-        System.out.println("printing via loop via generics:");
-        for(int i : s){
-            System.out.println(i);
-        }
-        System.out.println();
-        System.out.println("printing using Iterable:");
-        Iterator<Integer> val = s.iterator();
-        while(val.hasNext()){
-            System.out.println(val.next());
-        }
-
     }
 }
-
