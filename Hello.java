8e78436f07fd1f031cfeb8c26a6f91f366854c3b
Saurabh <devsaurabh086@gmail.com>
2025-10-11 20:53:00 +0530

    Map : Set-List combination

--- Hello.java
+++ Hello.java
@@ -4,6 +4,8 @@
 //List , Queue and Set are 3 interfaces that are extended by Collection interface.
 //List is implemented by class called ArrayList , and Set is implemented by class called HashSet().
 //If you wanna work with index , go with the list because set doesn't support index value.
+//Map : A combination of set and list where the keys can't be repeated but values can be.
+
 import java.util.*;
 
 class Hello{
@@ -50,12 +52,23 @@ class Hello{
         for(int i : s){
             System.out.println(i);
         }
-        System.out.println("Working with Iterable inteface:");
+        System.out.println("Working with Iterable interface:");
         System.out.println("Printing set values using Iterable:");
         Iterator<Integer> val = s.iterator();
         while(val.hasNext()){
             System.out.println(val.next());
         }
+        System.out.println("Basics of Map:");
+        Map<String , Integer> m = new HashMap<>();
+        m.put("Saurabh" , 88);
+        m.put("laukik" , 33);
+        m.put("shudhanshu" , 44);
+        m.put("non" , 23);
+        m.put("kashish" , 33);
+        m.put("rahul" , 88);
+        //m.put("Saurabh" , 78);
+        System.out.println("printing map directly:");
+        System.out.println(m);
 
     }
 }
