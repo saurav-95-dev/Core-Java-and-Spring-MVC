37d3d5a9e200f14a3acbe2028982b40dd1c4c6d2
Saurabh <devsaurabh086@gmail.com>
2025-10-13 23:42:26 +0530

    new change-01

--- Hello.java
+++ Hello.java
@@ -1,14 +1,26 @@
-import java.util.HashMap;
-import java.util.Map;
+hence we will get run-time exception :
+//Starting Collections :
+//->Starting with collection interface:
+//Collection belongs to java.util package.
+//Collections work with objects and not with the primitive values.
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 class Hello{
-    public static void main(String []args){
-        Map<String , Integer> m = new HashMap<>();
-        m.put("saurabh" , 23);
-        m.put("pragya" , 34);
-        System.out.println(m);
-        for(String s : m.keySet()){
-            System.out.println(s + " ---> " + m.get(s));
+    public static void main(String [] args){
+        Collection nums = new ArrayList();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+        nums.add(5);
+        nums.add("6"); //Here it will give run time exception.Hence, to prevent this , we use generics.
+        System.out.println("Printing the values directly:");
+        System.out.println(nums);
+        System.out.println("Printing using for loop:");
+        for (Object n : nums){
+            System.out.println((int)n);
         }
     }
 }
