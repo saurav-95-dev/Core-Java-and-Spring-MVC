e589c35f4f80f7b22bb55d7b28510adef57a7eb4
Saurabh <devsaurabh086@gmail.com>
2025-10-23 22:35:16 +0530

    Annotations

--- Hello.java
+++ Hello.java
@@ -1,39 +1,25 @@
-enum Laptop{
-    macbook(12334) , hp(123) , lenovo , acer(4546); //Named objects
+//Annotations and Types of Interface :
+//Annotations are meant for resolving the bugs at run time.
 
-    private int price;
+class A{
+    public void code(){
+        System.out.println("coding in a class ");
+    }
+}
 
-    private Laptop(){
-        System.out.println("default constructor called");
-        price = 1;
-    }
-    private Laptop(int price){
-        System.out.println("Parameterized constructor called for :" + this.name());
-        this.price = price;
-    }
+class B extends A{
 
-    //getters and setters:
-    public void setPrice(int p){
-        this.price = p;
+    @Override
+    public void code(){
+        System.out.println("coding in b class");
     }
-    public int getPrice(){
-        return price;
-    }
-
 }
 
 class Hello{
-    public static void main(String[] args){
 
-        Laptop l = Laptop.macbook;
-        System.out.println("Price of " + l + " is: " +l.getPrice());
-        System.out.println();
-        //Printing all laptop :
-        System.out.println("Printing all laptops:");
-        Laptop[] arr = Laptop.values();
-        for(Laptop i : arr){
-            System.out.println(i + "->" + i.ordinal() + " price -->" + i.getPrice() );
-        }
-
+    public static void main(String [] args){
+        A a = new B();
+        a.code();
     }
+
 }
