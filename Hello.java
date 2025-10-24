6dc481410ffdb331e26dea5b6ebff3297e30e696
Saurabh <devsaurabh086@gmail.com>
2025-10-24 23:32:31 +0530

    inner class and abstraction

--- Hello.java
+++ Hello.java
@@ -1,44 +1,26 @@
-//Types of interface :
-//Normal interface , functional interface (SAM), Marker .
-//Normal interface are the one with 2 or more methods .
-//Functional interface are the one with only one method declaration.
-//Marker interfaces are actually blank interface with no method.
-//**We can use lambda expression only with functional interface.
+//Inner class and abstraction :
 
-@FunctionalInterface //Annotations used here
-interface A{
-    void code(int i);
-}
-
-interface B{
-    int manage(int a , int b);
-}
-
-interface C{
-    public String testing(String name);
-}
-
-class Hello{
-
-    public static void main(String [] args){
-        A a = (int i)->{
-            System.out.println("code implemented in lamda expression-" + i);
-        };
-
-        a.code(123);
-
-        B b = (int x , int y)->{
-            return x+y;
-        };
-        int result  = b.manage(12,4);
-        System.out.println(result);
-
-        C c = (String name) ->{
-            return name;
-        };
-
-        String str = c.testing("Sapient");
-        System.out.println(str);
+abstract class A{
+    public abstract void code();
+    public void manage(){
+        System.out.println("Inside manage method under A class");
     }
+}
 
+public class Hello{
+    public static void main(String []args){
+        System.out.println("Inside main function");
+        A a = new A()
+        {
+
+            public void manage() {
+                System.out.println("Manage method changed");
+            }
+            public void code() {
+                System.out.println("Code is defined under anonymous inner class");
+            }
+        };
+        a.code();
+        a.manage();
+    }
 }
