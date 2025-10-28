124919054967f73ebf71053c4c53028fb30bc809
Saurabh <devsaurabh086@gmail.com>
2025-10-28 20:27:11 +0530

    compulsory assignment of value of the variable declared with var keyword

--- Hello.java
+++ Hello.java
@@ -1,10 +1,14 @@
 //Concept of LVTI --> Local variable type inference.
 //we can declare a variable using var keyword but only for a local variable and not instance variable
-
+//instance variable are for the storing purpose whereas local variable are for the processing purpose.
+//if we declare variable using var , we need to assign a value to it which is compulsory.
 
 class Hello{
     public static void main(String[] args){
         System.out.println("local variable type inference:");
-        
+        int i = 12;
+        var j = 15;
+        int c;
+
     }
 }
