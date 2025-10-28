5522a2e72c53dd93d33efc1b25d2f34d386aa623
Saurabh <devsaurabh086@gmail.com>
2025-10-28 20:34:49 +0530

    var in arrays and objects

--- Hello.java
+++ Hello.java
@@ -3,12 +3,20 @@
 //instance variable are for the storing purpose whereas local variable are for the processing purpose.
 //if we declare variable using var , we need to assign a value to it which is compulsory.
 
+class Student{
+
+}
 class Hello{
     public static void main(String[] args){
         System.out.println("local variable type inference:");
         int i = 12;
         var j = 15;
         int c;
+        //var in arrays and objects:
+        int arr[] = new int[12];
+        var arr1 = new int[10];
+
+        var obj = new Student();
 
     }
 }
