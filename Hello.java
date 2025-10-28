bde9b82da0848ca4b38370674b1b004a057c01ec
Saurabh <devsaurabh086@gmail.com>
2025-10-28 20:20:17 +0530

    concept of lvti

--- Hello.java
+++ Hello.java
@@ -1,19 +1,10 @@
-import java.util.*;
+//Concept of LVTI --> Local variable type inference.
+//we can declare a variable using var keyword but only for a local variable and not instance variable
 
-class Hello {
-    public static void main(String[] args) {
-        List<Integer> nums = List.of(1, 2, 3, 4, 5, 6, 7, 8);
 
-        System.out.println("Normal Stream:");
-        nums.stream()
-                .forEach(n -> {
-                    System.out.println(Thread.currentThread().getName() + " processed " + n);
-                });
+class Hello{
+    public static void main(String[] args){
+        System.out.println("local variable type inference:");
 
-        System.out.println("\nParallel Stream:");
-        nums.parallelStream()
-                .forEach(n -> {
-                    System.out.println(Thread.currentThread().getName() + " processed " + n);
-                });
     }
 }
