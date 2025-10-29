5cd79d04b44e348c59cbb09eb1d082b5e84895c2
Saurabh <devsaurabh086@gmail.com>
2025-10-29 22:50:43 +0530

    normal vs parallel stream

---
+++ Hello.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+class Hello {
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(1, 2, 3, 4, 5, 6, 7, 8);
+
+        System.out.println("Normal Stream:");
+        nums.stream()
+                .forEach(n -> {
+                    System.out.println(Thread.currentThread().getName() + " processed " + n);
+                });
+
+        System.out.println("\nParallel Stream:");
+        nums.parallelStream()
+                .forEach(n -> {
+                    System.out.println(Thread.currentThread().getName() + " processed " + n);
+                });
+    }
+}
