b08a5015171fe8a2e9ed107ad62f9ee5226454ba
Saurabh <devsaurabh086@gmail.com>
2025-11-02 23:40:30 +0530

    JUNIT-Initial-setup-01

--- Hello.java
+++ Hello.java
@@ -1,6 +1,4 @@
-//JUNIT5 :
-//-->Create a maven project-- will give environment to setup the JUNIT5 testing
-//Dependencies for testing are declared within pom.xml file.
+
 
 class Hello{
     public static void main(String []args){
