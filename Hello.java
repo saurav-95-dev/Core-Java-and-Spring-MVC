5e392a13d70548f53c4d618f7783cad6352bc71d
Saurabh <devsaurabh086@gmail.com>
2025-11-02 23:37:47 +0530

    work-space cleanup

--- Hello.java
+++ Hello.java
@@ -1,3 +1,9 @@
 //JUNIT5 :
 //-->Create a maven project-- will give environment to setup the JUNIT5 testing
 //Dependencies for testing are declared within pom.xml file.
+
+class Hello{
+    public static void main(String []args){
+        System.out.println("hii");
+    }
+}
