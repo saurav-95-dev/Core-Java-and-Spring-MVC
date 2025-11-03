06fd87e88e395506c0961d34253248b5e5f4e6f4
Saurabh <devsaurabh086@gmail.com>
2025-11-03 22:59:22 +0530

    Lambda expression basics

--- Hello.java
+++ Hello.java
@@ -1,25 +1,44 @@
-//Annotations and Types of Interface :
-//Annotations are meant for resolving the bugs at run time.
+//Types of interface :
+//Normal interface , functional interface (SAM), Marker .
+//Normal interface are the one with 2 or more methods .
+//Functional interface are the one with only one method declaration.
+//Marker interfaces are actually blank interface with no method.
+//**We can use lambda expression only with functional interface.
 
-class A{
-    public void code(){
-        System.out.println("coding in a class ");
-    }
+@FunctionalInterface //Annotations used here
+interface A{
+    void code(int i);
 }
 
-class B extends A{
+interface B{
+    int manage(int a , int b);
+}
 
-    @Override
-    public void code(){
-        System.out.println("coding in b class");
-    }
+interface C{
+    public String testing(String name);
 }
 
 class Hello{
 
     public static void main(String [] args){
-        A a = new B();
-        a.code();
+        A a = (int i)->{
+            System.out.println("code implemented in lamda expression-" + i);
+        };
+
+        a.code(123);
+
+        B b = (int x , int y)->{
+            return x+y;
+        };
+        int result  = b.manage(12,4);
+        System.out.println(result);
+
+        C c = (String name) ->{
+            return name;
+        };
+
+        String str = c.testing("Sapient");
+        System.out.println(str);
     }
 
 }
