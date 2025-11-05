95c1cd91958d569c0d7eefa441f87b6e65522599
Saurabh <devsaurabh086@gmail.com>
2025-11-05 22:56:29 +0530

    enums with constructor, ordinals and loop

--- Hello.java
+++ Hello.java
@@ -1,20 +1,39 @@
+enum Laptop{
+    macbook(12334) , hp(123) , lenovo , acer(4546); //Named objects
 
-import java.util.*;
+    private int price;
 
-class Hello {
-    public static void main(String[] args) {
-        List<Integer> nums = List.of(1, 2, 3, 4, 5, 6, 7, 8);
+    private Laptop(){
+        System.out.println("default constructor called");
+        price = 1;
+    }
+    private Laptop(int price){
+        System.out.println("Parameterized constructor called for :" + this.name());
+        this.price = price;
+    }
 
-        System.out.println("Normal Stream:");
-        nums.stream()
-                .forEach(n -> {
-                    System.out.println(Thread.currentThread().getName() + " processed " + n);
-                });
+    //getters and setters:
+    public void setPrice(int p){
+        this.price = p;
+    }
+    public int getPrice(){
+        return price;
+    }
 
+}
 
-        nums.parallelStream()
-                .forEach(n -> {
-                    System.out.println(Thread.currentThread().getName() + " processed " + n);
-                });
+class Hello{
+    public static void main(String[] args){
+
+        Laptop l = Laptop.macbook;
+        System.out.println("Price of " + l + " is: " +l.getPrice());
+        System.out.println();
+        //Printing all laptop :
+        System.out.println("Printing all laptops:");
+        Laptop[] arr = Laptop.values();
+        for(Laptop i : arr){
+            System.out.println(i + "->" + i.ordinal() + " price -->" + i.getPrice() );
+        }
+
     }
 }
