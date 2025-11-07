e224d641b731c3749576bb3e10eb775a99758503
Saurabh <devsaurabh086@gmail.com>
2025-11-07 23:16:35 +0530

    JUNIT-5-Initial

--- Hello.java
+++ Hello.java
@@ -1,29 +1,11 @@
-//Working of throws keyword :
-//NOTES:
-//throws is used when there is ducking of the exceptions , and it makes sense for checked exceptions.
+public class Hello {
 
-class A{
-    public void show(){
-
-        System.out.println("Inside show function");
-        try{
-            Class.forName("Demo");
-        }
-        catch (ClassNotFoundException e){
-            System.out.println("Sorry ..class not found anywhere...");
-        }
+    public static void main(String[] args) {
+        System.out.println("Inside main");
     }
-}
 
-class Hello{
-
-    static {
-        System.out.println("Main class is loaded...");
-    }
-    public static void main(String [] args){
-
-        System.out.println("Inside main function");
-        A a = new A();
-        a.show();
+    // method to test
+    public int addNumbers(int a, int b) {
+        return a + b;
     }
 }
