0a1ec7ba055bbbf3d7ca97d96d5114cc80cd88c9
Saurabh <devsaurabh086@gmail.com>
2026-06-25 09:55:32 +0530

    JDBC -- Insert Operation

--- JDBCLearning/src/main/java/com/dev/learning/JDBC.java
+++ JDBCLearning/src/main/java/com/dev/learning/JDBC.java
@@ -1,30 +1,28 @@
 package com.dev.learning;
 import java.sql.*;
 
-public class JDBC {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-         //Step 1 : Load and register the driver:
+class JDBC {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        //Step-1 : Load and register the driver:
         Class.forName("com.mysql.cj.jdbc.Driver");
 
-        //Step 2 : Establish connection to the database :
+        //Step-2 :
         String url = "jdbc:mysql://localhost:3306/jdbclearning";
-        String user = "root";
-        String password = "xyz";
-        Connection connect = DriverManager.getConnection(url , user , password);
-
-        //Step 3 : Create statement object upon which SQL query will be performed:
-        Statement statement = connect.createStatement();
-
-        //Step 4 : Execute the query on statement object:
-        //*Whenever we do on-select operation , we'll use executeUpdate() method:
-        String query = "insert into Student values(11 , 'Nikita' , 23 , 'Gorakhpur')";
-        int rows = statement.executeUpdate(query);
-        if(rows>0){
-            System.out.println("Student has been inserted successfully");
+        String username = "root";
+        String password = "xyz"; // your real password
+        Connection con =  DriverManager.getConnection(url, username, password);
+        //Ste-3 : Create Statement upon which query will be executed:
+        Statement st = con.createStatement();
+        String query = "insert into Student values(12 , 'Anmol' , 1 , 'Ranchi')";
+        int rows = st.executeUpdate(query);
+        if(rows == 0){
+            System.out.println("Operation failed");
         }
         else{
-            System.out.println("Student has NOT been inserted successfully");
+            System.out.println("Operation successful");
         }
+        st.close();
+        con.close();
 
 
     }
