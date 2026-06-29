1d1029042e340c1b6fef4c1b27090eed144b6706
Saurabh <devsaurabh086@gmail.com>
2026-06-29 20:50:36 +0530

    JDBC--Dao Design Pattern -- Fetch Operation with Exception handling & Helper class with user input

--- JDBCLearning/src/main/java/com/dev/learning/JdbcDao.java
+++ JDBCLearning/src/main/java/com/dev/learning/JdbcDao.java
@@ -1,6 +1,8 @@
 package com.dev.learning;
 
 import java.sql.SQLException;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 class Student{
     int id;
@@ -14,7 +16,9 @@ public class JdbcDao {
         //Business logic inside main function-
         StudentDao studentDao = new StudentDao();
         Student student = new Student();
-        int id = 11;
+        System.out.println("Enter the id of Student you want to fetch:");
+        Scanner sc = new Scanner(System.in);
+        int id = sc.nextInt();
         try {
             student = studentDao.getStudentById(id);
         } catch (SQLException e) {
